29b8c9c74d2c8336a83da73eef81a57d2ac1688d
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2026-05-23 21:05:06 +0530

    Added solution - LeetHub

---
+++ 701389--GFG/701389-.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int countDigits(int n) {
+        // Code here
+        if(n == 0) return 0;
+        int totalDigit = countDigits(n / 10);
+        return 1 + totalDigit;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
