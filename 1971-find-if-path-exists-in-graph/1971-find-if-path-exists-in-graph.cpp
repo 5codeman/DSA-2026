7bf29083e75c4f62f50fe1725b4c9917480614c1
CHIRAG RAJ <101505481+5codeman@users.noreply.github.com>
2026-07-05 03:16:11 +0530

    Time: 455 ms (23.26%), Space: 352.6 MB (10.94%) - LeetHub - Time: O(N + E), Space: O(N)

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,25 +1,44 @@
 class Solution {
 public:
-    bool checkPathByBFS(auto& graph, int source, int destination, int size) {
-        queue<int>qu;
-        vector<int>visited(size);
+    // bool checkPathByBFS(auto& graph, int source, int destination, int size) {
+    //     queue<int>qu;
+    //     vector<int>visited(size, 0);
+    //     visited[source] = 1;
+    //     qu.push(source);
+    //     while(!qu.empty()) {
+    //         int node = qu.front();
+    //         if(node == destination) return true;
+    //         qu.pop();
+    //         for(const auto& i : graph[node]) {
+    //             if(visited[i] != 1) {
+    //                 visited[i] = 1;
+    //                 qu.push(i);
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    bool checkPathByDFS(auto& graph, int source, int destination, int size, auto& visited) {
+        
         visited[source] = 1;
-        qu.push(source);
-        while(!qu.empty()) {
-            int node = qu.front();
-            if(node == destination) return true;
-            qu.pop();
-            for(const auto& i : graph[node]) {
-                if(visited[i] != 1) {
-                    visited[i] = 1;
-                    qu.push(i);
-                }
+
+        if(source == destination) return true;
+
+        for(const auto& i : graph[source]) {
+            if(visited[i] == 0) {
+                // visited[i] = 1; - ye repeted karne ka koi faida nahi hai kyunki recursive approach hai and bahar mai same kaam to ho hi raha hai.
+                auto check = checkPathByDFS(graph, i, destination, size, visited);
+                if(check == true) return true;
             }
         }
         return false;
     }
+
+
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         unordered_map<int, vector<int>>graph;
+        vector<int>visited(n, 0);
 
         for(int i = 0; i < edges.size(); i++) {
             int vertex1 = edges[i][0];
@@ -28,7 +47,7 @@ public:
             graph[vertex2].push_back(vertex1);
         }
 
-        return checkPathByBFS(graph, source, destination, n);
+        return checkPathByDFS(graph, source, destination, n, visited);
     }
 };
 
